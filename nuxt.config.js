9bf4d6f8a6bdaf95c9d6f6712e4289d026a2da9e
Netgen <matthewt@netgen.co.za>
2019-10-07 23:11:22 +0200

    Set-up the layout shell

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,25 @@ module.exports = {
   /*
    ** Nuxt.js modules
    */
-  modules: ["@nuxtjs/toast", "@nuxtjs/auth"],
+  modules: [
+    "@nuxtjs/toast",
+    "@nuxtjs/auth",
+    [
+      "nuxt-fontawesome",
+      {
+        imports: [
+          {
+            set: "@fortawesome/free-solid-svg-icons",
+            icons: ["fas"]
+          },
+          {
+            set: "@fortawesome/free-brands-svg-icons",
+            icons: ["fab"]
+          }
+        ]
+      }
+    ]
+  ],
 
   //Notifications
   toast: {
--- plugins/global.js
+++ plugins/global.js
@@ -1,5 +1,17 @@
+import Vue from "vue"
+
 //------------ PACKAGE DECLARATIONS
 import "gin-blocks"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 
 // ------------ STYLES
 import "../assets/styles/custom.css"
+
+// ------------ COMPONENTS
+import SidebarPlugin from "../components/SidebarPlugin/index"
+
+library.add(faUserSecret)
+Vue.component("font-awesome-icon", FontAwesomeIcon)
+Vue.use(SidebarPlugin)
