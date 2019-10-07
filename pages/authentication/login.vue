2d1ae85d7ee36301d99e89d0c5d888eab61797af
Matthew Troost <matthew.troost@hotmail.com>
2019-10-07 18:02:42 +0200

    Cleaned out components folder

--- pages/authentication/login.vue
+++ pages/authentication/login.vue
@@ -2,19 +2,6 @@
   <div class="row justify-content-center">
     <div class="col-lg-5 col-md-7">
       <div class="card bg-secondary shadow border-0">
-        <div class="card-header bg-transparent pb-5">
-          <div class="text-muted text-center mt-2 mb-3">
-            <small>Sign in with</small>
-          </div>
-          <div class="btn-wrapper text-center">
-            <a href="#" class="btn btn-neutral btn-icon">
-              <span class="btn-inner--icon">
-                <img src="~/assets/images/google.svg" />
-              </span>
-              <span class="btn-inner--text">Google</span>
-            </a>
-          </div>
-        </div>
         <div class="card-body px-lg-5 py-lg-5">
           <form role="form">
             <gin-input
