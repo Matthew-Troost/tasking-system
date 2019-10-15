d2d9970eaabe4df227dac56c78760f7a4c684c52
chris7818kbbf4 <chris1.sweatman@gmail.com>
2019-10-15 17:04:54 +0200

    login view

--- pages/authentication/login.vue
+++ pages/authentication/login.vue
@@ -1,5 +1,60 @@
 <template>
-  <div class="row justify-content-center">
+  <div class="auth-layout-wrap">
+    <div class="auth-content">
+      <div class="card o-hidden">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="p-4">
+              <div class="auth-logo text-center mb-30">
+                <img :src="model.logo" />
+              </div>
+              <h1 class="mb-3 text-18">Sign In</h1>
+              <b-form @submit.prevent="formSubmit">
+                <b-form-group label="Email Address" class="text-12">
+                  <b-form-input
+                    v-model="model.email"
+                    class="form-control-rounded"
+                    type="text"
+                    email
+                    required
+                  ></b-form-input>
+                </b-form-group>
+
+                <b-form-group label="Password" class="text-12">
+                  <b-form-input
+                    v-model="model.password"
+                    class="form-control-rounded"
+                    type="password"
+                  ></b-form-input>
+                </b-form-group>
+
+                <!-- <b-button block to="/" variant="primary btn-rounded mt-2"
+                  >Sign In</b-button
+                > -->
+                <b-button
+                  type="submit"
+                  tag="button"
+                  class="btn-rounded btn-block mt-2"
+                  variant="primary mt-2"
+                  @click="login"
+                >
+                  SignIn
+                </b-button>
+              </b-form>
+
+              <div class="mt-3 text-center">
+                <router-link to="forgot" tag="a" class="text-muted">
+                  <u>Forgot Password?</u>
+                </router-link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- <div class="row justify-content-center">
     <div class="col-lg-5 col-md-7">
       <div class="card bg-secondary shadow border-0">
         <div class="card-body px-lg-5 py-lg-5">
@@ -7,7 +62,7 @@
             <gin-input
               v-model="model.email"
               class="input-group-alternative mb-3"
-              placeholder="Email2"
+              placeholder="Email"
             ></gin-input>
 
             <gin-input
@@ -35,7 +90,7 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
 </template>
 <script>
 export default {
@@ -46,7 +101,8 @@ export default {
     return {
       model: {
         email: "",
-        password: ""
+        password: "",
+        logo: require("@../../../assets/images/logo2.png")
       }
     }
   },
@@ -94,3 +150,13 @@ export default {
   }
 }
 </script>
+<style scoped>
+.auth-layout-wrap .auth-content {
+  min-width: 25rem;
+}
+.auth-logo img {
+  /* height: 100px; */
+  width: unset;
+  margin-bottom: 1.5rem;
+}
+</style>
