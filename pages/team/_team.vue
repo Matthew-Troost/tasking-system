0391bdf00e3a5d82a2a24d27fb7e250ba781a5a2
chris7818kbbf4 <chris1.sweatman@gmail.com>
2019-10-18 10:28:43 +0200

    merge request changes

--- pages/team/_team.vue
+++ pages/team/_team.vue
@@ -15,53 +15,55 @@
         </b-form-input>
       </b-col>
     </b-row>
-    <!-- <keep-alive> -->
-    <b-row>
-      <b-col
-        v-for="user in users"
-        v-show="
-          user.type && user.type.includes(position) && searchUser(user.id)
-        "
-        :key="user.id"
-        lg="3"
-        sm="6"
-        md="4"
-        class="user-card"
-      >
-        <!-- start::profile -->
-        <b-card class="card-profile-1 mb-30 text-center">
-          <div class="avatar mb-3">
-            <img src="@/assets/images/avatars/matthewt.svg" alt />
-          </div>
-          <h5 class="m-0">{{ user.first_name }}</h5>
-
-          <div v-if="userProjects[user.id]">
-            <div
-              v-for="project in userProjects[user.id].map(x => x)"
-              :key="project"
-              class="text-center"
-            >
-              <b-badge pill variant="outline-dark p-2 m-1"
-                >{{ project }}
-              </b-badge>
+    <keep-alive>
+      <b-row>
+        <!-- <transition-group name="fade" tag="b-col"> -->
+        <b-col
+          v-for="user in users"
+          v-show="
+            user.type && user.type.includes(position) && searchUser(user.id)
+          "
+          :key="user.id"
+          lg="3"
+          sm="6"
+          md="4"
+          class="user-card"
+        >
+          <!-- start::profile -->
+          <b-card class="card-profile-1 mb-30 text-center">
+            <div class="avatar mb-3">
+              <img src="@/assets/images/avatars/matthewt.svg" alt />
             </div>
-          </div>
+            <h5 class="m-0">{{ user.first_name }}</h5>
 
-          <button class="btn btn-primary btn-rounded mt-2">
-            {{ user.first_name }}'s Schedule
-          </button>
-        </b-card>
-      </b-col>
-    </b-row>
-    <!-- </keep-alive> -->
+            <div v-if="userProjects[user.id]">
+              <div
+                v-for="project in userProjects[user.id].map(x => x)"
+                :key="project"
+                class="text-center"
+              >
+                <b-badge pill variant="outline-dark p-2 m-1"
+                  >{{ project }}
+                </b-badge>
+              </div>
+            </div>
+
+            <button class="btn btn-primary btn-rounded mt-2">
+              {{ user.first_name }}'s Schedule
+            </button>
+          </b-card>
+        </b-col>
+        <!-- </transition-group> -->
+      </b-row>
+    </keep-alive>
   </div>
 </template>
 <script>
 import { mapState } from "vuex"
+import Util from "@/utils"
 
 export default {
   layout: "default",
-  name: "Developers",
   data() {
     return {
       name: "",
@@ -87,6 +89,22 @@ export default {
     }
   },
   created() {
+    this.name = Util.linkToString(this.$route.params.team)
+    switch (this.name.toLowerCase()) {
+      case "developers":
+        this.position = "developer"
+        break
+      case "designers":
+        this.position = "designer"
+        break
+      case "managing":
+        this.position = "management"
+        break
+      case "socialmedia":
+        this.position = "socialmedia"
+        this.name = "Social Media"
+        break
+    }
     //fetch users
     this.$store.state.db.collection("users").onSnapshot(users => {
       if (users && users.docs) {
@@ -96,40 +114,22 @@ export default {
           })
         })
       }
-    }),
-      error => {
-        this.$toast.error(error, {
-          theme: "bubble",
-          position: "top-left",
-          duration: 5000
-        })
-      }
+    })
+
     //fetch projects
-    this.$store.state.db.collection("projects").onSnapshot(projects => {
-      if (projects && projects.docs) {
-        projects.docs.forEach(project => {
-          this.$store.commit("projects/setProject", {
-            project
-          })
-          //Populate userProjects array
-          const projectData = project.data()
-          if (projectData.lists) {
-            projectData.lists.forEach(list => {
-              if (list.tasks)
-                list.tasks.forEach(task => {
-                  if (task.users)
-                    task.users.forEach(user => {
-                      if (!this.userProjects[user.id]) {
-                        this.userProjects[user.id] = []
-                      }
-                      this.userProjects[user.id].push(projectData.name)
-                    })
-                })
+    this.$store.state.db.collection("projects").onSnapshot(
+      projects => {
+        if (projects && projects.docs) {
+          projects.docs.forEach(project => {
+            this.$store.commit("projects/setProject", {
+              project
             })
-          }
-        })
-      }
-    }),
+            //Populate userProjects array
+            const projectData = project.data()
+            this.addUserProject(projectData)
+          })
+        }
+      },
       error => {
         this.$toast.error(error, {
           theme: "bubble",
@@ -137,43 +137,37 @@ export default {
           duration: 5000
         })
       }
+    )
   },
-  mounted() {
-    switch (this.$route.params.team) {
-      case "developers":
-        this.position = "developer"
-        this.name = "Developers"
-        break
-      case "designers":
-        this.position = "designer"
-        this.name = "Designers"
-        break
-      case "managing":
-        this.position = "management"
-        this.name = "Managing"
-        break
-      case "socialmedia":
-        this.position = "socialmedia"
-        this.name = "Social Media"
-        break
-    }
-  },
+  mounted() {},
   methods: {
     searchUser(userId) {
       const user = this.users[userId]
       return (
         (user.first_name &&
-          user.first_name.toLowerCase().includes(this.searchWord)) ||
+          user.first_name
+            .toLowerCase()
+            .includes(this.searchWord.toLowerCase())) ||
         (user.last_name &&
-          user.last_name.toLowerCase().includes(this.searchWord))
+          user.last_name.toLowerCase().includes(this.searchWord.toLowerCase()))
       )
+    },
+    addUserProject(projectData) {
+      if (projectData.lists) {
+        projectData.lists.forEach(list => {
+          if (list.tasks)
+            list.tasks.forEach(task => {
+              if (task.users)
+                task.users.forEach(user => {
+                  if (!this.userProjects[user.id]) {
+                    this.userProjects[user.id] = []
+                  }
+                  this.userProjects[user.id].push(projectData.name)
+                })
+            })
+        })
+      }
     }
-  },
-  addUserProject(userId, projectName) {
-    if (!this.userProjects[userId]) {
-      this.userProjects[userId] = []
-    }
-    this.userProjects[userId].push(projectName)
   }
 }
 </script>
@@ -181,5 +175,12 @@ export default {
 <style scoped>
 .user-card {
   margin-bottom: 15px;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
