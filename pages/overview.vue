c4bcfe448365435e65adf349f4e067da92c8eda9
Matthew Troost <matthew.troost@hotmail.com>
2019-11-13 16:25:11 +0200

    Started on the layout

--- pages/overview.vue
+++ pages/overview.vue
@@ -1,35 +1,85 @@
 <template>
   <div>
+    <div class="avatar">
+      <img v-lazy="user == null ? '' : user.avatar" alt />
+      <h1>
+        {{ user == null ? "" : `${user.first_name} ${user.last_name}` }}
+      </h1>
+    </div>
     <b-row>
-      <b-col md="4">
-        <img v-lazy="url" alt />
+      <b-col md="3">
+        <b-card class="project-progress">
+          <div class="ul-widget-app__poll-list mb-4">
+            <div class="d-flex">
+              <span class="text-muted text-12">From UK</span>
+              <span class="text-muted text-12 ml-auto">
+                75%
+              </span>
+            </div>
+            <b-progress height="5px" :value="75"></b-progress>
+          </div>
+          <div class="ul-widget-app__poll-list mb-4">
+            <div class="d-flex">
+              <span class="text-muted text-12">From U.S.A</span>
+              <span class="text-muted text-12 ml-auto">
+                33%
+              </span>
+            </div>
+            <b-progress height="5px" variant="success" :value="33"></b-progress>
+          </div>
+          <div class="ul-widget-app__poll-list mb-4">
+            <div class="d-flex">
+              <span class="text-muted text-12">From Canada</span>
+              <span class="text-muted text-12 ml-auto">
+                49%
+              </span>
+            </div>
+            <b-progress height="5px" variant="dark" :value="49"></b-progress>
+          </div>
+        </b-card>
+        <b-card>
+          <div class="chart-wrapper" style="height: 300px ; width:100%">
+            <v-chart :options="echartPie" :autoresize="true"></v-chart>
+          </div>
+        </b-card>
       </b-col>
+      <b-col md="9"> <b-card> </b-card> </b-col>
     </b-row>
   </div>
 </template>
 <script>
-import { mapState } from "vuex"
+import { mapState, mapGetters } from "vuex"
 
 export default {
   layout: "default",
   name: "Dashboard",
   computed: {
+    ...mapGetters({
+      getUser: "users/getUserByUID"
+    }),
     ...mapState({
-      users: state => state.users.users
-    })
-  },
-  created() {
-    this.$store.state.db.collection("users").onSnapshot(users => {
-      let source = users.metadata.hasPendingWrites ? "Local" : "Server"
-      console.log(`Source: ${source}`)
-      if (users && users.docs) {
-        users.docs.forEach(user =>
-          this.$store.commit("users/setUser", {
-            user
-          })
-        )
-      }
-    })
+      currentUser: state => state.users.current_user
+    }),
+    user() {
+      return this.getUser(this.currentUser.uid)
+    }
   }
 }
 </script>
+<style scoped>
+.avatar {
+  margin-bottom: 20px;
+}
+.avatar img {
+  width: 120px;
+}
+.avatar h1 {
+  display: inline-block;
+  margin-bottom: 0px;
+  margin-left: 50px;
+  font-size: 50px;
+}
+.project-progress {
+  margin-bottom: 10px;
+}
+</style>
