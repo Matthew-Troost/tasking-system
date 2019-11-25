b78da4810f8e67b4ab6917e55537c16eca2f6a2c
Matthew Troost <matthewt@netgen.co.za>
2019-11-25 15:03:03 +0000

    Revert "Develop"
    This reverts commit ed5281b2a0de927307f28342dfb3a4c234b67a62

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,107 +1,52 @@
+
 module.exports = {
-  mode: "spa", //spa (single page application) is client-side rendered, universal is server-side
+  mode: 'spa',
   /*
-   ** Headers of the page
-   */
-  env: {
-    DEVELOPMENT: true, //switch this to true before deploying
-    SLACK_OAUTH:
-      "xoxp-823708849781-823708850917-825918728998-fb4382930818ce29838da6180a82e0d9",
-    SLACK_BOT_OAUTH: "xoxb-823708849781-818109654179-aw07enjx34ROM9zUzeYGfx1e"
-  },
+  ** Headers of the page
+  */
   head: {
-    title: process.env.npm_package_name || "",
+    title: process.env.npm_package_name || '',
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      {
-        hid: "description",
-        name: "description",
-        content: process.env.npm_package_description || ""
-      }
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
-  },
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: "#2f495e" },
-  /*
-   ** Global CSS
-   */
-  css: ["@/assets/styles/custom.css"],
-  /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: [
-    "@plugins/global.js",
-    "@plugins/authentication.js",
-    "@plugins/snapshotsubscriptions.js",
-    "@plugins/echarts.js",
-    process.env.DEVELOPMENT ? "" : "@plugins/bugsnag.js"
-  ],
-  /*
-   ** Nuxt.js dev-modules
-   */
-  buildModules: [],
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [
-    "@nuxtjs/toast",
-    "@nuxtjs/auth",
-    "@nuxtjs/axios",
-    [
-      "nuxt-fontawesome",
-      {
-        imports: [
-          {
-            set: "@fortawesome/free-solid-svg-icons",
-            icons: ["fas"]
-          },
-          {
-            set: "@fortawesome/free-brands-svg-icons",
-            icons: ["fab"]
-          }
-        ]
-      }
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
-  ],
-
-  axios: {
-    // proxyHeaders: false
-  },
-
-  //Notifications
-  toast: {
-    theme: "bubble",
-    position: "top-left",
-    duration: 5000
-  },
-
-  router: {
-    middleware: ["authenticationguard"],
-    linkActiveClass: "open"
   },
   /*
-   ** Build configuration
-   */
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+  /*
+  ** Build configuration
+  */
   build: {
-    transpile: ["vue-echarts", "resize-detector"],
-    extend(config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: "pre",
-          test: /\.(js|vue)$/,
-          loader: "eslint-loader",
-          exclude: /(node_modules)/
-        })
-      }
-      //chrome debugging
-      if (ctx.isDev) {
-        config.devtool = ctx.isClient ? "eval-source-map" : "inline-source-map"
-      }
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
     }
   }
 }
--- plugins/snapshotsubscriptions.js
+++
@@ -1,5 +0,0 @@
-export default ({ store }) => {
-  store.dispatch("projects/getAll")
-  store.dispatch("users/getAll")
-  store.dispatch("taskextras/getAll")
-}
