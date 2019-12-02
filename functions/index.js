43f76519843e69104105a041011548d68f794cd9
Matthew Troost <matthew.troost@hotmail.com>
2019-12-02 16:13:54 +0200

    Bound project assign to ticket cloud function

--- functions/index.js
+++ functions/index.js
@@ -38,6 +38,28 @@ admin.initializeApp()
 exports.receiveTicket = functions.https.onRequest(async (request, response) => {
   console.log("Receiving ticket...")
 
+  var projects = []
+  await admin
+    .firestore()
+    .collection("projects")
+    .get()
+    .then(snapshot => {
+      snapshot.forEach(doc => {
+        projects.push(doc.data())
+      })
+      return
+    })
+    .catch(err => {
+      console.error(`Issue fetching projects: ${err}`)
+      bugsnagClient.notify(`TICKET FUNCTION - Issue fetching projects: ${err}`)
+    })
+
+  var associatedProject = projects.find(project => {
+    return project.settings.associatedEmails.includes(request.body.from)
+  })
+
+  var attachments = []
+
   var thread = {
     from: request.body.from,
     to: request.body.to,
@@ -45,6 +67,13 @@ exports.receiveTicket = functions.https.onRequest(async (request, response) => {
     subject: request.body.subject,
     html: request.body.html,
     date: admin.firestore.Timestamp.fromDate(new Date(request.body.date))
+  }
+
+  //move this to seperate function and call after add or update
+  if (request.body.attachments) {
+    request.body.attachments.forEach(attachment => {
+      attachments.push(Buffer.from(attachment, "base64"))
+    })
   }
 
   await admin
@@ -56,13 +85,15 @@ exports.receiveTicket = functions.https.onRequest(async (request, response) => {
     .then(snapshot => {
       if (snapshot.empty) {
         console.log(`Received new ticket - ${request.body.ticketNumber}`)
+
         admin
           .firestore()
           .collection("tickets")
           .add({
             number: request.body.ticketNumber,
             status: "open",
-            thread: [thread]
+            thread: [thread],
+            projectId: associatedProject ? associatedProject.id : ""
           })
       } else {
         console.log("Ticket already opened, adding thread")
@@ -72,13 +103,19 @@ exports.receiveTicket = functions.https.onRequest(async (request, response) => {
           .doc(snapshot.docs[0].id)
           .update({
             status: "open",
-            thread: admin.firestore.FieldValue.arrayUnion(thread)
+            thread: admin.firestore.FieldValue.arrayUnion(thread),
+            projectId: snapshot.docs[0].projectId
+              ? snapshot.docs[0].projectId
+              : associatedProject
+              ? associatedProject.id
+              : ""
           })
       }
       return null
     })
     .catch(err => {
       console.error(err)
+      bugsnagClient.notify(`TICKET FUNCTION - ${err}`)
     })
 
   return response.status(200).send()
