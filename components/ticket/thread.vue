9ca6a7bfbe0e918e41a8f1e5979394ccc0502261
Matthew Troost <matthew.troost@hotmail.com>
2019-12-04 11:50:32 +0200

    Shelled out to threaditem component

--- components/ticket/thread.vue
+++ components/ticket/thread.vue
@@ -3,40 +3,22 @@
     <VclList v-if="!value"></VclList>
     <div v-else>
       <h3 class="subject">{{ value.thread[0].subject }}</h3>
-      <div
-        v-for="threaditem in value.thread"
+      <threadItem
+        v-for="(threaditem, index) in value.thread"
         :key="threaditem.id"
-        class="thread-content"
-      >
-        <div
-          class="details"
-          :class="[
-            threaditem.from.includes('@netgen.co.za')
-              ? 'details-staff'
-              : 'details-client'
-          ]"
-        >
-          <p>
-            Received:
-            <b>{{ threaditem.date.toDate() | moment("dddd, MMMM Do YYYY") }}</b>
-          </p>
-          <p>
-            From: <b>{{ threaditem.from }}</b>
-          </p>
-          <p v-if="threaditem.cc">Cc: {{ threaditem.cc }}</p>
-        </div>
-        <!-- eslint-disable-next-line vue/no-v-html -->
-        <div v-html="threaditem.html"></div>
-      </div>
+        v-model="value.thread[index]"
+      />
     </div>
   </b-card>
 </template>
 <script>
 import { VclList } from "vue-content-loading"
+import threadItem from "./threaditem"
 
 export default {
   components: {
-    VclList
+    VclList,
+    threadItem
   },
   props: {
     value: {
@@ -46,31 +28,3 @@ export default {
   }
 }
 </script>
-<style scoped>
-.thread-content {
-  border: 1px solid #a7a7a7;
-  border-radius: 5px;
-  padding: 15px;
-}
-.subject {
-  margin-bottom: 25px;
-}
-.details {
-  border-radius: 5px;
-  padding: 5px;
-  padding-left: 10px;
-  margin-bottom: 10px;
-}
-.details-client {
-  border: 1px solid #cecece;
-  background-color: #cecece;
-}
-.details-staff {
-  border: 1px solid #3f51b5;
-  background-color: #3f51b5;
-  color: white;
-}
-.details p {
-  margin-bottom: 0;
-}
-</style>
---
+++ components/ticket/threaditem.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="thread-content" :class="[showAll ? 'full-height' : '']">
+    <div
+      class="details"
+      :class="[
+        value.from.includes('@netgen.co.za')
+          ? 'details-staff'
+          : 'details-client'
+      ]"
+    >
+      <p>
+        Received:
+        <b
+          >{{ value.date.toDate() | moment("dddd, Do MMMM YYYY") }} at
+          {{ value.date.toDate() | moment("HH:mm") }}</b
+        >
+      </p>
+      <p>
+        From: <b>{{ value.from }}</b>
+      </p>
+      <p v-if="value.cc">Cc: {{ value.cc }}</p>
+    </div>
+    <div class="load-more">
+      <b-button variant="outline-light m-1 btn-sm" @click="toggleHeight"
+        >Show more</b-button
+      >
+    </div>
+    <!-- eslint-disable-next-line vue/no-v-html -->
+    <div v-html="value.html"></div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      type: Object,
+      default: null
+    }
+  },
+  data() {
+    return {
+      showAll: false
+    }
+  },
+  methods: {
+    toggleHeight() {
+      this.showAll = !this.showAll
+    }
+  }
+}
+</script>
+<style scoped>
+.thread-content {
+  border: 1px solid #a7a7a7;
+  border-radius: 5px;
+  padding: 15px;
+  max-height: 400px;
+  overflow: hidden;
+  -webkit-transition: 1s;
+  transition: 1s;
+  position: relative;
+  margin-bottom: 15px;
+}
+.thread-content:last-child {
+  margin-bottom: 0 !important;
+}
+.full-height {
+  max-height: unset;
+}
+.subject {
+  margin-bottom: 25px;
+}
+.details {
+  border-radius: 5px;
+  padding: 5px;
+  padding-left: 10px;
+  margin-bottom: 10px;
+}
+.details-client {
+  border: 1px solid #cecece;
+  background-color: #cecece;
+}
+.details-staff {
+  border: 1px solid #3f51b5;
+  background-color: #3f51b5;
+  color: white;
+}
+.details p {
+  margin-bottom: 0;
+}
+.load-more {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+  padding-bottom: 10px;
+  background: #ffffffd1;
+}
+</style>
